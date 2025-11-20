659adaf8e43eb09f4ec1dba9a63bc534d52a97e5
mehmetsisoglu <79602287+mehmetsisoglu@users.noreply.github.com>
2025-11-20 09:31:14 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
-import { StrictMode } from 'react'
+// React'ı açıkça import et (Rollup hatasını çözer)
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Dashboard from './Dashboard.jsx'
+// Uzantıyı kaldır (.jsx yazma, Vite kendisi bulur)
+import Dashboard from './Dashboard' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
