ef65771a27d8c4c5c7edc7600205d6b81b0f1126
mehmetsisoglu <79602287+mehmetsisoglu@users.noreply.github.com>
2025-11-20 11:37:45 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
+// src/main.jsx (Final Configuration for Dashboard URL)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; 
+import Dashboard from './Dashboard'; // Sadece Dashboard'u çağır
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> 
+    <Dashboard /> 
   </React.StrictMode>,
 );
