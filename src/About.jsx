c80d5ee7569f6959febabd09a3397e90965ecb49
mehmetsisoglu <79602287+mehmetsisoglu@users.noreply.github.com>
2025-11-20 11:27:00 +0300

    Create About.jsx

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div style={{ padding: '50px', color: 'white', backgroundColor: 'black' }}>
+            <h1>Hakkımızda Sayfası</h1>
+            <p>Bu bileşen, App.jsx dosyasındaki rotayı çalıştırmak için oluşturulmuştur.</p>
+        </div>
+    );
+};
+
+export default About;
