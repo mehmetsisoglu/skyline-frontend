590ebe36ab119cc1b746651cefb965b9a119fdf2
mehmetsisoglu <79602287+mehmetsisoglu@users.noreply.github.com>
2025-11-20 09:37:22 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // React modülünü bulamazsa diye elle gösteriyoruz
-      'react': 'react',
-      'react-dom': 'react-dom',
-    },
-  },
 })
