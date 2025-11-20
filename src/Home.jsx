197ef21f8430ef35d3e4a6832d9629adf050e708
mehmetsisoglu <79602287+mehmetsisoglu@users.noreply.github.com>
2025-11-20 11:26:43 +0300

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{ padding: '50px', color: 'white', backgroundColor: 'black' }}>
+            <h1>Ana Sayfa Yükleniyor...</h1>
+            <p>Lütfen bu sayfaya orijinal sitenizin içeriğini taşıyın.</p>
+        </div>
+    );
+};
+
+export default Home;
