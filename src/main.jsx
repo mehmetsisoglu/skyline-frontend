d7408897cf4c8a84af98f29c9c77bed759f86276
mehmetsisoglu <79602287+mehmetsisoglu@users.noreply.github.com>
2025-11-20 09:44:56 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Dashboard from './Dashboard' // .jsx uzantısını yazma
+import Dashboard from './Dashboard'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
